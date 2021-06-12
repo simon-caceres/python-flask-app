abec3c91507c7ba5a0c46695b28d9440f4ce495f
Simon Caceres <simon.caceres@egt.cl>
2021-06-12 19:46:57 -0400

    cambis en ruta probando post y otras caracteristicas antes de iniciar

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hola Mundo'
 
-@app.route('/new/<usuario>')
-def new(usuario):
-    return 'hola {}'.format(usuario)
+@app.route('/post/<post_id>', methods=['GET'])
+def get_post(post_id):
+    return 'el id del post es: {}'.format(post_id)
 
-@app.route('/route')
+@app.route('/post/<post_id>', methods=['POST'])
+def post_post(post_id):
+    return 'el id del post es: {}'.format(post_id)
+
+@app.route('/route', methods=['POST'])
 def route():
-    return 'route'
+    print(url_for('post_post', post_id = 2))
+    print(request.form['llave1'])
+    print(request.form['llave2'])
+    return 'recibimos tus datos'
