f72da733a3cd1943bed7649d9a7032d14a1f14f4
Simon Caceres <simon.caceres@egt.cl>
2021-06-12 21:45:12 -0400

    cambios para conectar con bdd y empezar a renderizar datos

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, request, url_for, redirect, abort
 from flask.templating import render_template
+from pathlib import Path
+import mysql.connector
+# settings.py
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 app = Flask(__name__)
+midb = mysql.connector.connect(
+    host='localhost',
+    user= os.environ.get('DB_USERNAME'),
+    password= os.environ.get('DB_PASSOWRD'),
+    database= os.environ.get('DB_BASE')
+)
+
+cursor = midb.cursor(dictionary=True)
 
 @app.route('/')
 def index():
@@ -16,14 +34,15 @@ def get_post(post_id):
             "email": "email.com"
         }
 
-
 @app.route('/route', methods=['POST', 'GET'])
 def route():
     #abort(403)
     #return redirect(url_for('get_post', post_id = 2))
     # print(request.form['llave1'])
     # print(request.form['llave2'])
-    return render_template('route.html')
+    cursor.execute('select * from usuarios')
+    usuarios = cursor.fetchall()
+    return render_template('route.html', usuarios = usuarios)
 
 @app.route('/home', methods=['GET'])
 def home():
